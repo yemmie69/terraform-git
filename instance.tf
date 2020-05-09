0a79c37175a66888ef677f2043e1ec7483c1209d
yemmie69 <tosin.olayiwola@southwark.gov.uk>
2020-05-09 07:17:01 +0100

    key pair

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "project" {
   count         = var.aws-count
   instance_type = var.instance_type
 //key_name = "${aws_key_pair.demo.key_name}"
-key_name = "packer"
+key_name = "terraform"
 
 
 
