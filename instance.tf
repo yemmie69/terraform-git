dfc6db3a70247e22a9cf2fd80f4a9c21396b4433
yemmie69 <tosin.olayiwola@southwark.gov.uk>
2020-05-19 22:27:09 +0100

    update

--- instance.tf
+++ instance.tf
@@ -1,15 +1,16 @@
 resource "aws_instance" "project" {
-  ami           = var.ami
-  count         = var.aws-count
-  instance_type = var.instance_type
+ // ami           = "${var.ami}"
+  ami             = "${var.region_ami["${var.region}"]}"
+  count         = "${var.aws-count}"
+  instance_type = "${var.instance_type}"
   //key_name = "${aws_key_pair.demo.key_name}"
   key_name = "terraform"
 
  
 
  tags = {
-    Name = "Jenkins ${count.index + 2}"
-
+    Name = "blue-box ${count.index + 1}"
+//Name= "jenkins ${count.index + 1}"
 
   }
 }
