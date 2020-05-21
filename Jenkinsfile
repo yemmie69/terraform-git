f2b5acb58fd8cfd4f738bd56afdf6c38f4698ace
yemmie69 <tosin.olayiwola@southwark.gov.uk>
2020-05-21 15:44:37 +0100

    update

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,7 +15,7 @@ pipeline {
          }
       }
       
-      stage('Terrafor apply') {
+      stage('Terraform apply') {
            environment {
               AWS_ACCESS_KEY_ID = credentials ("AWS_ACCESS_KEY_ID")
                AWS_SECRET_ACCESS_KEY = credentials ("AWS_SECRET_ACCESS_KEY")
--- variables.tf
+++ variables.tf
@@ -15,15 +15,14 @@ variable "instance_type" {
 variable "region_ami" {
  type = "map"
   default = {
-   eu-west-1 = "ami-096d3cd7ce28a6d6c" 
-   eu-west-1 = "ami-0fe87dbdf262b0943"
+  // eu-west-1 = "ami-0fe87dbdf262b0943"
   // eu-west-1 = "ami-05d0bd3a19163a540"
   
   }
 }
 
 variable "aws-count" {
-  default = 2
+  default = 1
 
 }
 
